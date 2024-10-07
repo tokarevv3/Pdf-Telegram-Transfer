f1d6041c1dc65505348d3f18a78e5b59ab1bb947
tokarevv3 <tokarev.0603@mail.ru>
2024-10-07 22:00:24 +0300

    1.1, readme

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Pdf-Telegram Transfer
+Required an application.properties file in "resources" folder which contains:
+1. path - A path to folder of new PDF-files.
+2. bot.token - Token for telegram bot.
+3. bot.chat - Id of chat of user and bot. May use "/chat" command to get id.
+
--- src/main/java/Telegram/TelegramSending.java
+++ src/main/java/Telegram/TelegramSending.java
@@ -3,6 +3,7 @@ package Telegram;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -15,16 +16,35 @@ public class TelegramSending implements LongPollingSingleThreadUpdateConsumer {
     private static final TelegramClient telegramClient = new OkHttpTelegramClient(PropertiesUtil.get("bot.token"));
 
     @Override
-    public void consume(Update update) {}
+    public void consume(Update update) {
+        if (update.getMessage().toString().equals("/chat")) {
+            String message = update.getMessage().getChatId().toString();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage msg = SendMessage
+                    .builder()
+                    .chatId(chatId)
+                    .text(message)
+                    .build();
+
+            try {
+                telegramClient.execute(msg);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void sendFile(String fileName) {
-        String filePath = new StringBuilder().append(PropertiesUtil.get("path"))
+        String filePath = new StringBuilder()
+                .append(PropertiesUtil.get("path"))
                 .append("/")
                 .append(fileName)
                 .toString();
         InputFile file = new InputFile(new File(filePath));
 
-        SendDocument doc = SendDocument.builder()
+        SendDocument doc = SendDocument
+                .builder()
                 .chatId(PropertiesUtil.get("bot.chat"))
                 .document(file).build();
         try {
